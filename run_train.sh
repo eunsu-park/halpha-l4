8e8f311ded5c7494fe2a686225ed6cfe58cd4c95
Eunsu Park <phd.choux@gmail.com>
2025-03-20 14:57:47 +0900

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,30 +1,30 @@
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode random \
-#     --num_inp 19 \
-#     --experiment_name random_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode random \
+    --num_inp 19 \
+    --experiment_name random_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode random \
-#     --num_inp 27 \
-#     --experiment_name random_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode random \
+    --num_inp 27 \
+    --experiment_name random_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode random \
-#     --num_inp 33 \
-#     --experiment_name random_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode random \
+    --num_inp 33 \
+    --experiment_name random_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode random \
-#     --num_inp 41 \
-#     --experiment_name random_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode random \
+    --num_inp 41 \
+    --experiment_name random_41 \
 
 
 ####
@@ -37,187 +37,187 @@ python train.py \
     --num_inp 19 \
     --experiment_name index_0_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 0 \
-#     --num_inp 27 \
-#     --experiment_name index_0_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 0 \
+    --num_inp 27 \
+    --experiment_name index_0_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 0 \
-#     --num_inp 33 \
-#     --experiment_name index_0_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 0 \
+    --num_inp 33 \
+    --experiment_name index_0_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 0 \
-#     --num_inp 41 \
-#     --experiment_name index_0_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 0 \
+    --num_inp 41 \
+    --experiment_name index_0_41 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 1 \
-#     --num_inp 19 \
-#     --experiment_name index_1_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 1 \
+    --num_inp 19 \
+    --experiment_name index_1_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 1 \
-#     --num_inp 27 \
-#     --experiment_name index_1_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 1 \
+    --num_inp 27 \
+    --experiment_name index_1_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 1 \
-#     --num_inp 33 \
-#     --experiment_name index_1_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 1 \
+    --num_inp 33 \
+    --experiment_name index_1_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 1 \
-#     --num_inp 41 \
-#     --experiment_name index_1_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 1 \
+    --num_inp 41 \
+    --experiment_name index_1_41 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 2 \
-#     --num_inp 19 \
-#     --experiment_name index_2_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 2 \
+    --num_inp 19 \
+    --experiment_name index_2_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 2 \
-#     --num_inp 27 \
-#     --experiment_name index_2_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 2 \
+    --num_inp 27 \
+    --experiment_name index_2_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 2 \
-#     --num_inp 33 \
-#     --experiment_name index_2_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 2 \
+    --num_inp 33 \
+    --experiment_name index_2_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 2 \
-#     --num_inp 41 \
-#     --experiment_name index_2_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 2 \
+    --num_inp 41 \
+    --experiment_name index_2_41 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 3 \
-#     --num_inp 19 \
-#     --experiment_name index_3_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 3 \
+    --num_inp 19 \
+    --experiment_name index_3_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 3 \
-#     --num_inp 27 \
-#     --experiment_name index_3_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 3 \
+    --num_inp 27 \
+    --experiment_name index_3_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 3 \
-#     --num_inp 33 \
-#     --experiment_name index_3_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 3 \
+    --num_inp 33 \
+    --experiment_name index_3_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 3 \
-#     --num_inp 41 \
-#     --experiment_name index_3_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 3 \
+    --num_inp 41 \
+    --experiment_name index_3_41 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 4 \
-#     --num_inp 19 \
-#     --experiment_name index_4_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 4 \
+    --num_inp 19 \
+    --experiment_name index_4_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 4 \
-#     --num_inp 27 \
-#     --experiment_name index_4_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 4 \
+    --num_inp 27 \
+    --experiment_name index_4_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 4 \
-#     --num_inp 33 \
-#     --experiment_name index_4_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 4 \
+    --num_inp 33 \
+    --experiment_name index_4_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 4 \
-#     --num_inp 41 \
-#     --experiment_name index_4_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 4 \
+    --num_inp 41 \
+    --experiment_name index_4_41 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 5 \
-#     --num_inp 19 \
-#     --experiment_name index_5_19 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 5 \
+    --num_inp 19 \
+    --experiment_name index_5_19 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 5 \
-#     --num_inp 27 \
-#     --experiment_name index_5_27 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 5 \
+    --num_inp 27 \
+    --experiment_name index_5_27 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 5 \
-#     --num_inp 33 \
-#     --experiment_name index_5_33 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 5 \
+    --num_inp 33 \
+    --experiment_name index_5_33 \
 
-# python train.py \
-#     --data_dir /NAS/data/halpha/dataset \
-#     --output_dir /NAS/result/halpha \
-#     --data_mode index \
-#     --test_index 5 \
-#     --num_inp 41 \
-#     --experiment_name index_5_41 \
+python train.py \
+    --data_dir /NAS/data/halpha/dataset \
+    --output_dir /NAS/result/halpha \
+    --data_mode index \
+    --test_index 5 \
+    --num_inp 41 \
+    --experiment_name index_5_41 \
 
